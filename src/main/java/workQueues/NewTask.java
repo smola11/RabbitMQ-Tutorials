92181b1e889801ac8c485df0b01a4429e60719f9
Smolarz <mjsz@gft.com>
2018-07-16 11:12:48 +0200

    Added NewTask class for sending mesaages created based on provided arguments

---
+++ src/main/java/workQueues/NewTask.java
@@ -0,0 +1,46 @@
+package workQueues;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String message = getMessage(args);
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+
+    private static String getMessage(String[] strings) {
+        if (strings.length < 1) {
+            return "Hello World";
+        }
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimeter) {
+        int length = strings.length;
+        if (length == 0) return "";
+        StringBuilder words = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimeter).append(strings[i]);
+        }
+        return words.toString();
+    }
+
+}
